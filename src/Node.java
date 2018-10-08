525cd70d6ba35d125b4b51a5cd51a198fa022bc5
eoghanmcdermott <eoghan.mc-dermott.1@ucdconnect.ie>
2018-10-08 14:55:17 +0000

    Added some rough notes based on the tutorial

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,21 @@
 //Eoghan McDermott - 15345451
 
+//constructing a tree that will give us a specific T value when searched to its full depth
+//E value should be opposite of T value (wait maybe no)
+//don't store T value for interior nodes - use estimates
+//only have leaf nodes store T values
+// interior nodes hold estimated values of leaf nodes - add some noise to T values
+//make sure the branching factor isn't static
+//maybe use a vector to store nodes?
+//no need to put parent nodes in there(uh oh?)
+//RECURSION - :^(
+//estimate and not the real value
+//make sure you don't change the tree
+//can ignore the bit about placing the daughter randomly - don't change their ordering
+//don't change the tree when you're searching it - maybe pass as an argument
+//since we're using random numbers we don't need any test cases - do it manually basically
+//make sure you properly generate the random numbers, inclusive, uniform distribution
+
 import java.util.ArrayList;
 
 public class Node {
@@ -21,6 +37,13 @@ public class Node {
             return false;
         else
             return true;
+    }
+
+    public boolean hasSiblings(){//might use this
+        if(parent.getChildren().size() > 1)
+            return true;
+        else
+            return false;
     }
 
     public void addChildren(ArrayList<Node> children){
